1c57c8a8e08969c9de1badfb502b733f8cd6db83
febin1123 <febin123.luv@gmail.com>
2018-07-17 11:48:04 +0530

    day2 commit 2

---
+++ pyth/bigger.py
@@ -0,0 +1,12 @@
+a=int(input('Enter I number: '))
+b=int(input('Enter II number: '))
+c=int(input('Enter III number: '))
+if (a>b and a>c):
+	print(a)
+elif(b>c):
+    print(b)
+else:
+	print(c)
+
+
+	
---
+++ pyth/one.py
@@ -0,0 +1,13 @@
+import sys
+print(sys.version)
+print('Random String!')
+obj=10
+print(obj)
+#if else statement:
+if obj>20:
+	print('Greater')
+elif obj>15:
+	print('Middle')
+else:
+    print('Smaller')
+print('Outside ifelse')    
